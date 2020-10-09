02f1288636d005744469d549dab9af03f2624098
Mahdiyeh_Rezaei <mahdiyehrezaei4@gmail.com>
2020-10-09 15:52:17 +0330

    added OrderRepository & ProductRepository .

--- service/business/product/src/main/java/com/mapsa/webstore/product/repository/OrderRepository.java
+++ service/business/product/src/main/java/com/mapsa/webstore/product/repository/OrderRepository.java
@@ -4,5 +4,5 @@ import com.mapsa.webstore.product.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    Order findByCart(Long cartId);
 }
