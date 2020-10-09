04111426c2e91b4579f9985e92bc47361e1a1dc3
1qazXSW@ <e.sadeghi@fwutech.com>
2020-10-09 10:33:39 +0330

    Merge Product service

---
+++ service/business/basicinfo/src/main/java/com/mapsa/webstore/basicinfo/BasicinfoApplication.java
@@ -0,0 +1,13 @@
+package com.mapsa.webstore.basicinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicinfoApplication.class, args);
+    }
+
+}
---
+++ service/business/basicinfo/src/test/java/com/mapsa/webstore/basicinfo/BasicinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapsa.webstore.basicinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
