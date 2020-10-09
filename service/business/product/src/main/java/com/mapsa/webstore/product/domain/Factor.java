8022783935c46aa6107727b662ff627824145787
Mahdiyeh_Rezaei <mahdiyehrezaei4@gmail.com>
2020-10-09 21:04:59 +0330

    Code optimization

--- service/business/product/src/main/java/com/mapsa/webstore/product/domain/Factor.java
+++ service/business/product/src/main/java/com/mapsa/webstore/product/domain/Factor.java
@@ -1,55 +1,23 @@
 package com.mapsa.webstore.product.domain;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 import javax.persistence.*;
 import java.util.Objects;
 
+@Data
+@Accessors(chain = true)
 @Entity
 public class Factor {
-    private long id;
-    private Long total;
-    private OrderTbl orderTblByOrderTblId;
-
     @Id
     @Column(name = "ID")
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
+    private long id;
     @Basic
     @Column(name = "TOTAL")
-    public Long getTotal() {
-        return total;
-    }
-
-    public void setTotal(Long total) {
-        this.total = total;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Factor factor = (Factor) o;
-        return id == factor.id &&
-                Objects.equals(total, factor.total);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, total);
-    }
-
+    private Long total;
     @ManyToOne
     @JoinColumn(name = "ORDER_TBL_ID", referencedColumnName = "ID")
-    public OrderTbl getOrderTblByOrderTblId() {
-        return orderTblByOrderTblId;
-    }
+    private OrderTbl orderTblByOrderTblId;
 
-    public void setOrderTblByOrderTblId(OrderTbl orderTblByOrderTblId) {
-        this.orderTblByOrderTblId = orderTblByOrderTblId;
-    }
 }
--- service/business/product/src/main/java/com/mapsa/webstore/product/domain/Product.java
+++ service/business/product/src/main/java/com/mapsa/webstore/product/domain/Product.java
@@ -5,87 +5,22 @@ import java.util.Objects;
 
 @Entity
 public class Product {
-    private long id;
-    private String name;
-    private Long price;
-    private String avatar;
-    private Long stock;
-    private GroupTbl groupTblByGroupId;
-
     @Id
     @Column(name = "ID")
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
+    private long id;
     @Basic
     @Column(name = "NAME")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    private String name;
     @Basic
     @Column(name = "PRICE")
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
+    private Long price;
     @Basic
     @Column(name = "AVATAR")
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
+    private String avatar;
     @Basic
     @Column(name = "STOCK")
-    public Long getStock() {
-        return stock;
-    }
-
-    public void setStock(Long stock) {
-        this.stock = stock;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return id == product.id &&
-                Objects.equals(name, product.name) &&
-                Objects.equals(price, product.price) &&
-                Objects.equals(avatar, product.avatar) &&
-                Objects.equals(stock, product.stock);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, price, avatar, stock);
-    }
-
+    private Long stock;
     @ManyToOne
     @JoinColumn(name = "GROUP_ID", referencedColumnName = "ID")
-    public GroupTbl getGroupTblByGroupId() {
-        return groupTblByGroupId;
-    }
-
-    public void setGroupTblByGroupId(GroupTbl groupTblByGroupId) {
-        this.groupTblByGroupId = groupTblByGroupId;
-    }
+    private GroupTbl groupTblByGroupId;
 }
