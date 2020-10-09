e3dfdcde8d593ad5d09840d08ce8b1abe262ae2c
Hossein Seyfoori <h.seyfoori@gmail.com>
2020-10-09 11:26:05 +0330

    add:eureka-server

---
+++ service/logic/eureka-server/src/test/java/com/mapsa/webstore/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapsa.webstore.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
