a2bc37a3971d87a93b243cab44d61a7c918f0da1
1qazXSW@ <e.sadeghi@fwutech.com>
2020-10-09 10:52:24 +0330

    init customer project

---
+++ service/business/customer/src/test/java/com/mapsa/webstore/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapsa.webstore.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
