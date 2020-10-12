f3e17471be0fd1100705af575cb6488d15632e57
soroush <soroush.un@gmail.com>
2020-10-12 10:45:09 +0330

    edit Dto

--- service/business/basicinfo/src/main/java/com/mapsa/webstore/basicinfo/dto/CountryDto.java
+++ service/business/basicinfo/src/main/java/com/mapsa/webstore/basicinfo/dto/CountryDto.java
@@ -6,7 +6,7 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class CountryDto {
-    //"Country","Alpha-2 code","Alpha-3 code","Numeric code","Latitude (average)","Longitude (average)"
+    //"Country","Alpha-2 code","Alpha-3 code","Numeric code""
     private String countryName;
     private String alphabet2Code ;
     private String alphabet3Code ;
